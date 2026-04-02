7c302fee01fae73aff808d88bafe5965487929a9
melroy12 <melroydsilva4@gmail.com>
2026-04-02 09:17:20 +0900

    update ast

--- peguses_lang/src/ast.rs
+++ peguses_lang/src/ast.rs
@@ -1,31 +1,67 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expr {
     Number(i64),
+    Boolean(bool),
     Ident(String),
     Binary {
         op: BinOp,
         left: Box<Expr>,
         right: Box<Expr>,
     },
+    Unary {
+        op: UnaryOp,
+        expr: Box<Expr>,
+    },
 }
 
+#[derive(Debug, Clone)]
+pub enum UnaryOp {
+    Not,
+    Neg,  // Unary minus
+}
 
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum BinOp {
+    // Arithmetic
     Add,
     Sub,
     Mul,
     Div,
+    Mod,
+    
+    // Comparison
+    Equal,
+    NotEqual,
+    Less,
+    Greater,
+    LessEqual,
+    GreaterEqual,
+    
+    // Logical
+    And,
+    Or,
 }
 
-#[derive(Debug)]
-pub enum Stmt{
+#[derive(Debug, Clone)]
+pub enum Stmt {
     Let {
+        name: String,
+        value: Expr,
+    },
+    Assign {
         name: String,
         value: Expr,
     },
     Print {
         value: Expr,
+    },
+    If {
+        condition: Expr,
+        then_block: Vec<Stmt>,
+        else_block: Option<Vec<Stmt>>,
+    },
+    While {
+        condition: Expr,
+        body: Vec<Stmt>,
     },
 }
