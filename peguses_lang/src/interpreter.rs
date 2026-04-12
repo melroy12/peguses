0171c9aaf845563e247373d21330f435b61d7260
melroy12 <melroydsilva4@gmail.com>
2026-04-12 17:05:19 +0900

    errpr ;s

--- peguses_lang/src/interpreter.rs
+++ peguses_lang/src/interpreter.rs
@@ -88,12 +88,12 @@ impl Interpreter {
         Ok(())
     }
 
-    fn execute(&mut self, stmt: &Stmt) -> Result<()> {
+    fn execute(&mut self, stmt: &Stmt) -> Result<LoopControl> {
         match stmt {
             Stmt::Let { name, value } => {
                 let result = self.eval_expr(value)?;
                 self.env.insert(name.clone(), result);
-                Ok(())
+                Ok(LoopControl::None)
             }
             Stmt::Assign { name, value } => {
                 if !self.env.contains_key(name) {
@@ -101,12 +101,12 @@ impl Interpreter {
                 }
                 let result = self.eval_expr(value)?;
                 self.env.insert(name.clone(), result);
-                Ok(())
+                Ok(LoopControl::None)
             }
             Stmt::Print { value } => {
                 let result = self.eval_expr(value)?;
                 println!("{}", result);
-                Ok(())
+                Ok(LoopControl::None)
             }
             Stmt::If {
                 condition,
@@ -116,14 +116,20 @@ impl Interpreter {
                 let cond_value = self.eval_expr(condition)?;
                 if cond_value.is_truthy() {
                     for stmt in then_block {
-                        self.execute(stmt)?;
+                        let ctrl = self.execute(stmt)?;
+                        if ctrl != LoopControl::None {
+                            return Ok(ctrl);
+                        }
                     }
                 } else if let Some(else_stmts) = else_block {
                     for stmt in else_stmts {
-                        self.execute(stmt)?;
+                        let ctrl = self.execute(stmt)?;
+                        if ctrl != LoopControl::None {
+                            return Ok(ctrl);
+                        }
                     }
                 }
-                Ok(())
+                Ok(LoopControl::None)
             }
             Stmt::While { condition, body } => {
                 loop {
@@ -132,11 +138,37 @@ impl Interpreter {
                         break;
                     }
                     for stmt in body {
-                        self.execute(stmt)?;
+                        let ctrl = self.execute(stmt)?;
+                        match ctrl {
+                            LoopControl::Break => return Ok(LoopControl::None),
+                            LoopControl::Continue => break,
+                            LoopControl::None => {},
+                        }
                     }
                 }
-                Ok(())
+                Ok(LoopControl::None)
             }
+            Stmt::For { var, start, end, body } => {
+                let start_val = self.eval_expr(start)?.as_number()
+                    .map_err(|e| format!("For loop start: {}", e))?;
+                let end_val = self.eval_expr(end)?.as_number()
+                    .map_err(|e| format!("For loop end: {}", e))?;
+                
+                for i in start_val..end_val {
+                    self.env.insert(var.clone(), Value::Number(i));
+                    for stmt in body {
+                        let ctrl = self.execute(stmt)?;
+                        match ctrl {
+                            LoopControl::Break => return Ok(LoopControl::None),
+                            LoopControl::Continue => break,
+                            LoopControl::None => {},
+                        }
+                    }
+                }
+                Ok(LoopControl::None)
+            }
+            Stmt::Break => Ok(LoopControl::Break),
+            Stmt::Continue => Ok(LoopControl::Continue),
         }
     }
 
@@ -145,6 +177,8 @@ impl Interpreter {
             Expr::Number(n) => Ok(Value::Number(*n)),
             
             Expr::Boolean(b) => Ok(Value::Boolean(*b)),
+            
+            Expr::String(s) => Ok(Value::String(s.clone())),
 
             Expr::Ident(name) => {
                 self.env
@@ -172,12 +206,17 @@ impl Interpreter {
                 match op {
                     // Arithmetic operators
                     BinOp::Add => {
-                        let l = left_val.as_number()
-                            .map_err(|e| format!("Addition error: left operand - {}", e))?;
-                        let r = right_val.as_number()
-                            .map_err(|e| format!("Addition error: right operand - {}", e))?;
-                        Ok(Value::Number(l.checked_add(r)
-                            .ok_or_else(|| "Addition error: integer overflow".to_string())?))
+                        // Support both number addition and string concatenation
+                        match (&left_val, &right_val) {
+                            (Value::Number(l), Value::Number(r)) => {
+                                Ok(Value::Number(l.checked_add(*r)
+                                    .ok_or_else(|| "Addition error: integer overflow".to_string())?))
+                            }
+                            (Value::String(l), Value::String(r)) => {
+                                Ok(Value::String(format!("{}{}", l, r)))
+                            }
+                            _ => Err("Addition error: operands must be both numbers or both strings".to_string())
+                        }
                     }
                     BinOp::Sub => {
                         let l = left_val.as_number()
@@ -260,6 +299,15 @@ impl Interpreter {
                     }
                     BinOp::Or => {
                         Ok(Value::Boolean(left_val.is_truthy() || right_val.is_truthy()))
+                    }
+                    
+                    // String operators
+                    BinOp::Concat => {
+                        let l = left_val.as_string()
+                            .map_err(|e| format!("Concatenation error: left operand - {}", e))?;
+                        let r = right_val.as_string()
+                            .map_err(|e| format!("Concatenation error: right operand - {}", e))?;
+                        Ok(Value::String(format!("{}{}", l, r)))
                     }
                 }
             }
